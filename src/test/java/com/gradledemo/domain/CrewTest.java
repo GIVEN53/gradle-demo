bcdf80f1562ef993abe8679b9d1f543c91e00a5c
Nam Gi Beom <rlqja0523@naver.com>
2024-07-15 01:16:32 +0900

    test: add test for demo

---
+++ src/test/java/com/gradledemo/domain/CrewTest.java
@@ -0,0 +1,26 @@
+package com.gradledemo.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CrewTest {
+
+    @Test
+    void 크루를_생성한다() {
+        Crew crew = new Crew("prin");
+        assertThat(crew.getName()).isEqualTo("prin");
+    }
+
+    private static class Crew {
+        private final String name;
+
+        public Crew(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+}
