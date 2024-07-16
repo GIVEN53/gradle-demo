086367ded74aa3b1b467fe71ce01747e7dcf6f1f
Nam Gi Beom <rlqja0523@naver.com>
2024-07-17 02:53:48 +0900

    test: add parallel test

---
+++ src/test/java/com/gradledemo/parallel/Parallel3Test.java
@@ -0,0 +1,17 @@
+package com.gradledemo.parallel;
+
+import org.junit.jupiter.api.RepeatedTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parallel3Test {
+
+    @RepeatedTest(1000)
+    void 병렬_테스트_3_실행() {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
